736ec74e9680434a3ecf8c669385667dcea5579a
munenari <munenari@gmail.com>
2021-05-17 20:07:07 +0900

    Change to writer

--- response/intercepter.go
+++ response/intercepter.go
@@ -8,12 +8,15 @@ import (
 // Intercepter will intercept http.responseWriter
 type Intercepter struct {
 	header     http.Header
-	Listener   chan []byte
 	StatusCode int
+
+	pr *io.PipeReader
+	pw *io.PipeWriter
 }
 
-func NewInterceptor(q chan []byte) *Intercepter {
-	return &Intercepter{Listener: q}
+func NewInterceptor() *Intercepter {
+	pr, pw := io.Pipe()
+	return &Intercepter{pr: pr, pw: pw}
 }
 
 // Header is returning header map
@@ -26,8 +29,22 @@ func (x *Intercepter) Header() http.Header {
 
 // Write will intercept bytes
 func (x Intercepter) Write(buf []byte) (int, error) {
-	x.Listener <- buf
-	return len(buf), nil
+	return x.pw.Write(buf)
+}
+
+// WriteClose will close writer
+func (x Intercepter) WriteClose() error {
+	return x.pw.Close()
+}
+
+// Read from intercept bytes
+func (x Intercepter) Read(buf []byte) (int, error) {
+	return x.pr.Read(buf)
+}
+
+// ReadClose will close reader
+func (x Intercepter) ReadClose() error {
+	return x.pr.Close()
 }
 
 // WriteHeader will set own status code
@@ -44,5 +61,6 @@ var (
 	// check interface
 	_ http.ResponseWriter = &Intercepter{}
 	_ io.Writer           = &Intercepter{}
+	_ io.Reader           = &Intercepter{}
 	_ http.CloseNotifier  = &Intercepter{}
 )
--- response/interceptor_test.go
+++ response/interceptor_test.go
@@ -1,16 +1,20 @@
 package response
 
 import (
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
+	"strings"
 	"testing"
 
 	echo "github.com/labstack/echo/v4"
 )
 
 func TestInterceptor(t *testing.T) {
-	q := make(chan []byte)
-	x := NewInterceptor(q)
+	// q := make(chan []byte)
+	x := NewInterceptor()
 	w8 := make(chan struct{})
 
 	t.Run("write response", func(tt *testing.T) {
@@ -22,12 +26,17 @@ func TestInterceptor(t *testing.T) {
 		if _, err := r.Write([]byte("foobar")); err != nil {
 			tt.Fatal(err)
 		}
+		x.WriteClose()
 		<-w8
 	})
 
 	t.Run("assert response", func(tt *testing.T) {
 		tt.Parallel()
-		b := <-q
+		defer x.ReadClose()
+		b, err := ioutil.ReadAll(x)
+		if err != nil {
+			tt.Fatal(err)
+		}
 		if x.StatusCode != 200 {
 			tt.Fatal("status code was unexpected,", x.StatusCode)
 		}
@@ -42,15 +51,20 @@ func TestInterceptor(t *testing.T) {
 }
 
 func TestInterceptorWithPipe(t *testing.T) {
-	q := make(chan []byte)
-	x := NewInterceptor(q)
+	// q := make(chan []byte, 10240)
+	// respr, respw := io.Pipe()
+	x := NewInterceptor()
 	w8 := make(chan struct{})
 	pr, pw := io.Pipe()
 
 	t.Run("write to pipe", func(tt *testing.T) {
 		tt.Parallel()
 		defer pw.Close()
-		pw.Write([]byte("foobar"))
+		sr := strings.NewReader(strings.Repeat("foobar12", 128*10)) // 8bytes * 128 = 1kb
+		_, err := io.Copy(pw, sr)
+		if err != nil {
+			tt.Fatal(err)
+		}
 	})
 
 	t.Run("write response", func(tt *testing.T) {
@@ -64,16 +78,22 @@ func TestInterceptorWithPipe(t *testing.T) {
 			tt.Fatal(n, err)
 		}
 		pr.Close()
+		x.WriteClose()
 		<-w8
 	})
 
 	t.Run("assert response", func(tt *testing.T) {
 		tt.Parallel()
-		b := <-q
+		// b := <-q
+		defer x.ReadClose()
+		b, err := ioutil.ReadAll(x)
+		if err != nil {
+			tt.Fatal(err)
+		}
 		if x.StatusCode != 200 {
 			tt.Fatal("status code was unexpected,", x.StatusCode)
 		}
-		if string(b) != "foobar" {
+		if string(b) != strings.Repeat("foobar12", 128*10) {
 			tt.Fatal("response body was unexpected,", string(b))
 		}
 		if x.Header().Get("X-Custom-Header-2") != "two" {
@@ -81,4 +101,53 @@ func TestInterceptorWithPipe(t *testing.T) {
 		}
 		w8 <- struct{}{}
 	})
+}
+
+func TestHTTPServer(t *testing.T) {
+	e := echo.New()
+	mid := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			orgWriter := c.Response().Writer
+			w := NewInterceptor()
+			c.Response().Writer = w
+			w8 := make(chan error)
+			go func() {
+				body, err := ioutil.ReadAll(w)
+				if err != nil {
+					w8 <- err
+					return
+				}
+				//
+				fmt.Println(string(body)) // Save to database
+				c.Response().Writer = orgWriter
+				for k, v := range w.Header() {
+					for _, s := range v {
+						c.Response().Header().Add(k, s)
+					}
+				}
+				c.Response().WriteHeader(w.StatusCode)
+				c.Response().Write(body)
+				w8 <- nil
+			}()
+			if err := next(c); err != nil {
+				return err
+			}
+			return <-w8
+		}
+	}
+	e.GET("/dummy", func(c echo.Context) error {
+		sr := strings.NewReader(strings.Repeat("foobar12", 128*10))
+		return c.Stream(http.StatusOK, "application/octet-stream", sr)
+	}, mid)
+	s := httptest.NewServer(e)
+	resp, err := s.Client().Get(s.URL + "/dummy")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer resp.Body.Close()
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Fatal(string(b))
 }
