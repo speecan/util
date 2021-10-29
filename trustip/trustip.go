11198837e392d1957374b20563626e7c388bd8fd
munenari <munenari@gmail.com>
2021-10-29 13:03:55 +0900

    Add ip list cacher

--- trustip/trustip.go
+++ trustip/trustip.go
@@ -1,25 +1,58 @@
 package trustip
 
 import (
-	"log"
+	"fmt"
 	"net"
+)
+
+type (
+	List struct {
+		IPs   []net.IP
+		CIDRs []*net.IPNet
+	}
 )
 
 // IsContained check if IP is within list
 func IsContained(ip string, list []string) bool {
+	l, _ := NewList(list)
+	return l.Contains(ip)
+}
+
+func NewList(list []string) (*List, error) {
+	l := &List{
+		IPs:   make([]net.IP, 0),
+		CIDRs: make([]*net.IPNet, 0),
+	}
+	var reterr error
 	for _, v := range list {
-		_, network, err := net.ParseCIDR(v)
-		if err == nil {
-			ip := net.ParseIP(ip)
-			if network.Contains(ip) {
-				return true
-			}
-		} else {
-			if net.ParseIP(ip).Equal(net.ParseIP(v)) {
-				return true
-			}
+		if ip := net.ParseIP(v); ip != nil { // ip
+			l.IPs = append(l.IPs, ip)
+			continue
+		}
+		_, cidr, err := net.ParseCIDR(v) // cidr
+		if err != nil {
+			reterr = fmt.Errorf("%s was neither IP nor CIDR: %w", v, err)
+			continue
+		}
+		l.CIDRs = append(l.CIDRs, cidr)
+	}
+	return l, reterr
+}
+
+func (x *List) Contains(ip string) bool {
+	t := net.ParseIP(ip)
+	if t == nil {
+		return false
+	}
+	for _, v := range x.CIDRs {
+		if v.Contains(t) {
+			return true
 		}
 	}
-	log.Println(ip + ": is not trusted")
+	for _, v := range x.IPs {
+		if v.Equal(t) {
+			return true
+		}
+	}
 	return false
 }
--- trustip/trustip_test.go
+++ trustip/trustip_test.go
@@ -1,6 +1,7 @@
 package trustip
 
 import (
+	"net"
 	"testing"
 )
 
@@ -30,4 +31,118 @@ func TestIsContained(t *testing.T) {
 	if res == true {
 		t.Errorf("%s: must be not parsed", ip)
 	}
+}
+
+func TestListContains(t *testing.T) {
+	t.Parallel()
+	list := []string{
+		"192.168.100.50",
+		"192.168.100.0/24",
+	}
+	l, err := NewList(list)
+	if err != nil {
+		t.Fatal(err)
+	}
+	ip := "192.168.100.50"
+	res := l.Contains(ip)
+	if res == false {
+		t.Errorf("%s: must be contained in list", ip)
+	}
+	ip = "192.168.100.51"
+	res = l.Contains(ip)
+	if res == false {
+		t.Errorf("%s: must be contained in list", ip)
+	}
+	ip = "192.168.101.50"
+	res = l.Contains(ip)
+	if res == true {
+		t.Errorf("%s: must be not contained in list", ip)
+	}
+	ip = "192.168.101.50//100"
+	res = l.Contains(ip)
+	if res == true {
+		t.Errorf("%s: must be not parsed", ip)
+	}
+}
+
+func BenchmarkIsContained(b *testing.B) {
+	list := []string{
+		"10.0.99.10",
+		"10.0.254.0/16",
+		"10.0.90.0/16",
+		"10.0.80.0/16",
+		"10.0.70.0/16",
+		"10.0.60.0/16",
+		"10.0.50.0//16",
+		"10.0.40.0/16",
+		"192.168.100.50",
+		"192.168.100.0/24",
+	}
+	b.Run("ip", func(bb *testing.B) {
+		ip := "192.168.100.50"
+		bb.ResetTimer()
+		for i := 0; i < bb.N; i++ {
+			IsContained(ip, list)
+		}
+	})
+	b.Run("cidr", func(bb *testing.B) {
+		ip := "192.168.100.51"
+		bb.ResetTimer()
+		for i := 0; i < bb.N; i++ {
+			if !IsContained(ip, list) {
+				bb.Fatal("ip must be contained")
+			}
+		}
+	})
+}
+
+func BenchmarkListContains(b *testing.B) {
+	list := []string{
+		"10.0.99.10",
+		"10.0.254.0/16",
+		"10.0.90.0/16",
+		"10.0.80.0/16",
+		"10.0.70.0/16",
+		"10.0.60.0/16",
+		"10.0.50.0//16",
+		"10.0.40.0/16",
+		"192.168.100.50",
+		"192.168.100.0/24",
+	}
+	l, _ := NewList(list)
+	b.Run("ip", func(bb *testing.B) {
+		ip := "192.168.100.50"
+		bb.ResetTimer()
+		for i := 0; i < bb.N; i++ {
+			if !l.Contains(ip) {
+				bb.Fatal("ip must be contained")
+			}
+		}
+	})
+	b.Run("cidr", func(bb *testing.B) {
+		ip := "192.168.100.51"
+		bb.ResetTimer()
+		for i := 0; i < bb.N; i++ {
+			if !l.Contains(ip) {
+				bb.Fatal("ip must be contained")
+			}
+		}
+	})
+}
+
+func BenchmarkIsPrivate(b *testing.B) {
+	b.Run("in private", func(bb *testing.B) {
+		ip := "192.168.100.50"
+		bb.ResetTimer()
+		for i := 0; i < bb.N; i++ {
+			net.ParseIP(ip).IsPrivate()
+		}
+	})
+	b.Run("no private", func(bb *testing.B) {
+		ip := "200.168.100.50"
+		bb.ResetTimer()
+		for i := 0; i < bb.N; i++ {
+			net.ParseIP(ip).IsPrivate()
+		}
+	})
 }
